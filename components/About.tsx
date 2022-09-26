33f7f792fd6eac072d63cd33d79ba6f4a8319a02
Jonathan <jtellkampf@gmail.com>
2022-09-26 22:48:54 +0100

    Completed about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,11 +1,49 @@
+import { motion } from "framer-motion";
+
 type Props = {};
 
 export default function About({}: Props) {
   return (
-    <div className="flex flex-col relative h-screen text-center md-text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
-    </div>
+
+      <motion.img
+        initial={{ x: -200, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1.2 }}
+        className="mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+        src="https://images.techopedia.com/images/uploads/web-developer.jpg"
+      />
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{" "}
+          <span className="underline decoration-[#f7ab0a]/50">little</span>{" "}
+          background
+        </h4>
+        <p className="text-base">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores vero
+          nobis sapiente nihil, atque corporis accusamus, molestiae repudiandae
+          tempora enim, debitis nesciunt. Iusto laudantium iure magni rerum
+          cupiditate nostrum nulla veniam consectetur itaque totam mollitia
+          omnis aspernatur obcaecati officiis, possimus porro id illum vel, modi
+          alias ad iste voluptate. Expedita. Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Consectetur mollitia modi nisi eaque,
+          officia nostrum unde saepe obcaecati dolores aperiam reprehenderit
+          incidunt sint deserunt, fugiat blanditiis voluptatum aspernatur
+          repellendus doloribus. Tempora quibusdam et, corporis aperiam minus
+          similique laudantium ad ratione nihil! Harum ab, sed rem distinctio
+          voluptate facilis tenetur vel.
+        </p>
+      </div>
+    </motion.div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
   return (
@@ -22,6 +23,10 @@ const Home: NextPage = () => {
 
       <section id="about" className="snap-center">
         <About />
+      </section>
+
+      <section id="experience" className="snap-center">
+        <WorkExperience />
       </section>
     </div>
   );
