0d59dd4e05b6ff858dbecddd4fcee895caaa402d
Jonathan <jtellkampf@gmail.com>
2022-09-27 18:37:48 +0100

    completed projects section

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import WorkExperience from "../components/WorkExperience";
 import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 
 const Home: NextPage = () => {
   return (
@@ -32,6 +33,10 @@ const Home: NextPage = () => {
 
       <section id="skills" className="snap-start">
         <Skills />
+      </section>
+
+      <section id="projects" className="snap-start">
+        <Projects />
       </section>
     </div>
   );
